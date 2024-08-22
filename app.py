6092b7ccd8d7803f3e0e98b29d44f6805571356f
Luis Vasquez <wizsebastian@gmail.com>
2024-08-22 16:50:00 -0400

    first real commit.

---
+++ app.py
@@ -0,0 +1,12 @@
+class GitHubManager:
+    def __init__(self, user_name: str):
+        self.user_name = user_name
+
+
+def main():
+    print("work from main")
+
+
+if __name__ == "__main__":
+    user = "wizsebastian"
+    main()
